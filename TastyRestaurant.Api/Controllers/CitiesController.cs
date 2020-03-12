d3ba1e798307603758cf42375a1f73ad39007758
Javier Reyes <reyesj02@stepstone.com>
2020-03-12 15:14:53 -0600

    Adding the Cities Controller and returning dummy json

---
+++ TastyRestaurant.Api/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TastyRestaurant.Api.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]//common base route for all the action methods
+    public class CitiesController : ControllerBase //not adding support for views
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult( 
+            new List<object>
+            {
+                new {id = 1, Name = "New York"},
+                new {id = 2, Name = "Antwerp"}
+            });
+        }
+    }
+}
