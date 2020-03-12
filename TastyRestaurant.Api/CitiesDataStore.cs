46fbd038c0049076547ec0938c730516beab1db1
Javier Reyes <reyesj02@stepstone.com>
2020-03-12 15:37:00 -0600

    Adding the GetCity Action method and some in memory data

---
+++ TastyRestaurant.Api/CitiesDataStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using TastyRestaurant.Api.Models;
+
+namespace TastyRestaurant.Api
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();    
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>
+            {
+                new CityDto {Id = 1, Name = "New York City", Description = "The one with the big park"},
+                new CityDto {Id = 2, Name = "San Francisco", Description = "The city with a beautiful bridge"},
+                new CityDto {Id = 3, Name = "Buenos Aires", Description = "The city where tango is danced"}
+            };
+        }
+    }
+}
--- TastyRestaurant.Api/Controllers/CitiesController.cs
+++ TastyRestaurant.Api/Controllers/CitiesController.cs
@@ -13,12 +13,15 @@ namespace TastyRestaurant.Api.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult( 
-            new List<object>
-            {
-                new {id = 1, Name = "New York"},
-                new {id = 2, Name = "Antwerp"}
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
+
+
 }
