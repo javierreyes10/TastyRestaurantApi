2ea658342d33a6cfc68ff5772d67cb01dd4efee1
Javier Reyes <javier.reyes10@outlook.com>
2020-03-29 21:46:20 -0600

    Added connectionString configuration

--- TastyRestaurant.Api/Startup.cs
+++ TastyRestaurant.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
@@ -17,6 +18,12 @@ namespace TastyRestaurant.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _congiruation;
+
+        public Startup(IConfiguration congiruation)
+        {
+            _congiruation = congiruation;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -62,7 +69,7 @@ namespace TastyRestaurant.Api
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
-            var connectionString = @"Server=(localdb)\MSSQLLocalDB;DataBase=CityInfoDb;Trusted_Connection=True";
+            var connectionString = _congiruation["connectionStrings:cityInfoDBConnectionString"];
 
             services.AddDbContext<CityInfoContext>(o =>
             {
